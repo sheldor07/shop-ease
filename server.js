137accebd0367aba0f93849f6cddc07940c1b931
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-12-30 21:44:37 +0530

    installed express,sequelize and started server
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,9 @@
+const express=require('express')
+
+const app=express()
+
+app.use('/',express.static(__dirname+"/public"))
+app.get('/')
+app.listen("4545", ()=>{
+    console.log("server started on localhost:4545")
+})
