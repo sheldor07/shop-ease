8095dee927a2064d5e922d279b9e98f02f68bcb2
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-12-30 21:54:09 +0530

    created user and product class in sequelize
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- db.js
+++ db.js
@@ -1,3 +1,46 @@
-const Sequelize=require('sequelize')
+const Sequelize = require('sequelize')
 
-const db=new Sequelize()
+//connecting db to sequelize
+const db = new Sequelize("shoppingdb","shopper","shoppingpass",{
+    host:"localhost",
+    dialect:"mysql",
+    pool:{
+        min:0,
+        max:2,
+    }
+})
+//we're creating a class called User and a class called Product
+const User = db.define('users',{
+    id:{
+        type:Sequelize.DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+    },
+    name:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull:false
+
+    }
+})
+const Product = db.define('products',{
+    id:{
+        type:Sequelize.DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+    },
+    name:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull:false
+
+    },
+    manufacturer:{
+        type:Sequelize.DataTypes.STRING,
+    },
+    price:{
+        defaultValue:0.0,
+        type:Sequelize.DataTypes.FLOAT,
+        allowNull:false,
+    }
+})
+
+exports = module.exports({User,Product})
